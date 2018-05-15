fd4565e7d8968c6f49531b498c67ee1037e96967
Vector5233 <vector5233@gmail.com>
2018-05-15 16:25:52 -0400

    Robot class added Drive class added, needs methods.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
@@ -10,8 +10,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class Drive extends Object {
 
     Robot sully;
-    OpMode opmode;
-    public Drive(Robot s, OpMode op) {
+    LinearOpMode opmode;
+    final double TICKS_PER_INCH = 1170 / (3.14 * 4);
+
+    public Drive(Robot s, LinearOpMode op) {
 
         sully = s;
         opmode = op;
@@ -29,13 +31,36 @@ public class Drive extends Object {
         setDriveMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
+    public void setPower(double power) {
+        sully.frontLeft.setPower(power);
+        sully.backLeft.setPower(power);
+        sully.frontRight.setPower(power);
+        sully.backRight.setPower(power);
+    }
+
     public void straightForDistance(double distance, double power) {
         /**
          * go forward (distance > 0) or backward (distance < 0) for distance in inches at
-         * given power level.
+         * given power level.  Assumes left motors are set to FORWARD, right motors are REVERSED
          */
+
+        int targetPosition = (int) (distance * TICKS_PER_INCH);
+        stopBot();
+        opmode.opModeIsActive();
+        setDriveMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        sully.frontLeft.setTargetPosition(targetPosition);
+        sully.backLeft.setTargetPosition(targetPosition);
+        sully.backRight.setTargetPosition(-targetPosition);
+        sully.frontRight.setTargetPosition(-targetPosition);
+
+        while (sully.frontLeft.isBusy() && sully.frontRight.isBusy()) {
+            ;
+        }
+
+        stopBot();
+
     }
 
-
-
 }
+
